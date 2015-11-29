a9537df381038903c6d0ac7546cad83bdc1446ef
Manuel <mlagunas94@gmail.com>
2015-11-29 17:54:54 +0100

    Calculo interseccion refractado

--- TrazadorRayos/src/Plano.java
+++ TrazadorRayos/src/Plano.java
@@ -1,4 +1,4 @@
- import java.awt.Color;
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -15,13 +15,14 @@ public class Plano implements Objeto {
 	boolean isMirror;
 	boolean isTransparent;
 
-	public Plano(ModeloLuz m, double d, float x, float y, float z, Color color, boolean mirror, boolean transparent) {
-		this.color= color;
+	public Plano(ModeloLuz m, double d, float x, float y, float z, Color color,
+			boolean mirror, boolean transparent) {
+		this.color = color;
 		this.m = m;
 		this.N = new Vector3D(x, y, z);
 		this.d = d;
-		this.isMirror=mirror;
-		this.isTransparent=transparent;
+		this.isMirror = mirror;
+		this.isTransparent = transparent;
 	}
 
 	public Plano(ModeloLuz m, Vector3D v, double d) {
@@ -36,59 +37,56 @@ public class Plano implements Objeto {
 	// d = 0
 	// nx * dx + ny * dy + nz * dz = N · D
 	// t = - (nx * ox + ny * oy + nz * oz + d) / (nx * dx + ny * dy + nz * dz)
-	/*@Override
-	public boolean intersect(Rayo r) {
-		Vector3D o = r.origin;
-		Vector3D d = r.direction;
-		double a = (n.x * o.x + n.y * o.y + n.z * o.z) + this.d;
-		double b = (n.x * d.x + n.y * d.y + n.z * d.z);
+	/*
+	 * @Override public boolean intersect(Rayo r) { Vector3D o = r.origin;
+	 * Vector3D d = r.direction; double a = (n.x * o.x + n.y * o.y + n.z * o.z)
+	 * + this.d; double b = (n.x * d.x + n.y * d.y + n.z * d.z);
+	 * 
+	 * if (b == 0) return false; double t = -a / b; // if(t<0) // return false;
+	 * return true;
+	 * 
+	 * }
+	 */
 
-		if (b == 0)
+	/**
+	 * Find the intersection of the plane and a given ray.
+	 *
+	 * The return value is positive is the intersection is found and this value
+	 * gives the distance along the ray. Negative values imply that the
+	 * intersection was either not successful or the intersection point was
+	 * before the origin. This value can be used with the pointAt method of the
+	 * Ray class (@see Ray#pointAt)
+	 *
+	 * @param ray
+	 *            the ray to intersect with
+	 * @return a <code>double</code> value that gives the distance along the
+	 *         ray.
+	 */
+	public boolean intersect(Rayo ray) {
+		Vector3D tmp;
+		double d1, d2, t;
+
+		tmp = new Vector3D(ray.origin.x, ray.origin.y, ray.origin.z);
+
+		/* Find angle between plane and direction of ray */
+		d2 = Vector3D.dotProd(ray.direction, N);
+
+		/* Is ray parallel to plane */
+		if (Math.abs(d2) < 0.00000001) {
 			return false;
-		double t = -a / b;
-		// if(t<0)
-		// return false;
-		return true;
+		}
 
-	}*/
-	
-	  /**
-	    * Find the intersection of the plane and a given ray.
-	    *
-	    * The return value is positive is the intersection is found and
-	    * this value gives the distance along the ray. Negative values
-	    * imply that the intersection was either not successful or the
-	    * intersection point was before the origin. This value can be used
-	    * with the pointAt method of the Ray class (@see Ray#pointAt)
-	    *
-	    * @param ray the ray to intersect with 
-	    * @return a <code>double</code> value that gives the distance
-	    * along the ray. 
-	    */
-	  public boolean intersect(Rayo ray) {
-	    Vector3D tmp; 
-	    double d1,d2,t;
-
-	    tmp = new Vector3D (ray.origin.x, ray.origin.y, ray.origin.z);
-
-	    /* Find angle between plane and direction of ray*/
-	    d2 = Vector3D.dotProd(ray.direction, N);
-		
-	    /* Is ray parallel to plane */
-	    if (Math.abs(d2) < 0.00000001) {
-	      return false;
-	    }
-		
-	    d1 = Vector3D.dotProd(tmp,N);
-	    t = (d - d1)/d2;
+		d1 = Vector3D.dotProd(tmp, N);
+		t = (d - d1) / d2;
 		ray.t = t;
 		ray.object = this;
-	    return true;
-	  }
+		return true;
+	}
 
 	@Override
 	public Color Shade(Rayo r, ArrayList<Luz> lights,
-			ArrayList<Objeto> objects, Color bgnd, int nRayos,double currentRefr) {
+			ArrayList<Objeto> objects, Color bgnd, int nRayos,
+			double currentRefr) {
 		// 0. (r) opuesto de L
 
 		// 1. (p) Punto de intersección rayo-objeto
@@ -105,36 +103,44 @@ public class Plano implements Objeto {
 				-r.direction.z);
 
 		// 4. (v) Rayo al ojo
-		Vector3D v = new Vector3D(px - r.origin.x, py - r.origin.y, pz - r.origin.z);
+		Vector3D v = new Vector3D(px - r.origin.x, py - r.origin.y, pz
+				- r.origin.z);
 
-		Vector3D ref =null;
-		if (isMirror){
+		Vector3D ref = null;
+		if (isMirror) {
 			// 5. (ref) Rayo reflejado
 			ref = r.origin.reflect(N);
 		}
-		
+
 		Vector3D frac = null;
-		if(isTransparent){ //Snell: sin(i)/sin(r) = nr/ni
+		if (isTransparent) { // Snell: sin(i)/sin(r) = nr/ni
 			// 6. (frac) Rayo refractado
-			
-			double NiNr=currentRefr/m.index; 
-			double cosI=-Vector3D.dotProd(N, r.direction);
-			double cosR=Math.sqrt(1.0-((1.0-(cosI*cosI))*(NiNr*NiNr)));
-			
-			if (cosR>0.0){
-//				 frac = Vector3D.add(Vector3D.scale(NiNr,r.direction),Vector3D.scale((NiNr*cosI)-cosR, n));
-				frac=Vector3D.sub(Vector3D.scale((NiNr*cosI-Math.sqrt(1-NiNr*NiNr*(1-(cosI*cosI)))),N),Vector3D.scale(NiNr,r.direction)); 
+
+			double NiNr = currentRefr / m.index;
+			double cosI = -Vector3D.dotProd(N, r.direction);
+			double cosR = Math
+					.sqrt(1.0 - ((1.0 - (cosI * cosI)) * (NiNr * NiNr)));
+
+			if (cosR > 0.0) {
+				// frac =
+				// Vector3D.add(Vector3D.scale(NiNr,r.direction),Vector3D.scale((NiNr*cosI)-cosR,
+				// n));
+				frac = Vector3D.sub(
+						Vector3D.scale(
+								(NiNr * cosI - Math.sqrt(1 - NiNr * NiNr
+										* (1 - (cosI * cosI)))), N),
+						Vector3D.scale(NiNr, r.direction));
 				frac.normalize();
-			}
-			else{
-				frac=null;
+			} else {
+				frac = null;
 			}
 		}
-		
-		
+
 		// The illumination model is applied
 		// by the surface's Shade() method
-		return m.calculo(color,bgnd, lights, objects, l, p, N, v, r.origin, isMirror,ref, isTransparent,frac, nRayos,currentRefr);
+		return m.calculo(color, bgnd, lights, objects, l, p, null, N, v,
+				r.origin, isMirror, ref, isTransparent, frac, nRayos,
+				currentRefr);
 	}
 
 }
